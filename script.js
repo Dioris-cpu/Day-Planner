1309fd56ef9cd86db05607d757ed54578f29e86c
Dioris Diaz <diorid@gmail.com>
2019-12-16 03:57:04 -0500

    functions in action

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ $(document).ready;
 setInterval(function () {
     var now = moment();
     $(".saveBtn").on("click", function () {
-   console.log(this);
-   var value = $(this).siblings(".description").val();
-   var time =  $(this).siblings(".hour").text();
-   localStorage.setItem(time, value);
-});
+        console.log(this);
+        var value = $(this).siblings(".description").val();
+        var time = $(this).siblings(".hour").text();
+        localStorage.setItem(time, value);
+    });
     $("#currentDay").text(now);
 }, 1000);
+
+
