e39bb675ed9361e6cf756a6a9b52a2feb18c9df9
Dioris Diaz <diorid@gmail.com>
2019-12-17 05:31:13 -0500

    local storage is almost working fully

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var firstElt = document.getElementById("first");
+
+
 $(document).ready;
 
 setInterval(function () {
@@ -7,7 +10,19 @@ setInterval(function () {
         var value = $(this).siblings(".description").val();
         var time = $(this).siblings(".hour").text();
         localStorage.setItem(time, value);
+        if(time && value){
+            localStorage.setItem(time,value);
+            location.reload()
+        }
+        for(var i = 0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+
+            firstElt.innerHTML += '${key}: ${value}<br />';
+
+        }    
     });
+   
     $("#currentDay").text(moment().format("llll"));
 });
 
